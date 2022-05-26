bacd8c2a6f78b255c4f081e14e8c3d5daa6d8c84
Marcus L. Jensen <lomztein4@gmail.com>
2022-05-26 10:07:24 +0200

    pretty major issues found and fixed

---
+++ stopwatch.py
@@ -0,0 +1,46 @@
+import time
+
+__name__ = "stopwatch"
+
+class Stopwatch:
+    start_time = 0
+    end_time = 0
+
+    def __init__(self, start_now = False):
+        if (start_now):
+            self.start()
+
+    def start(self):
+        self.start_time = time.time()
+
+    def end(self):
+        self.end_time = time.time()
+        return self.elapsed()
+
+    def reset(self):
+        self.end_time = 0
+        self.start_time = 0
+
+    def end_and_print(self):
+        self.end()
+        self.print()
+        return self.elapsed()
+
+    def end_and_printf(self):
+        self.end()
+        self.printf()
+        return self.elapsed()
+
+    def elapsed(self):
+        return self.end_time - self.start_time
+
+    def print(self):
+        print(self.elapsed())
+
+    def printf(self):
+        sec = self.elapsed()
+        mins = sec // 60
+        sec = sec % 60
+        hours = mins // 60
+        mins = mins % 60
+        print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
