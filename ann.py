3188b783e6b32d87c813d45501e164e5b0a8515a
Marcus L. Jensen <lomztein4@gmail.com>
2022-05-29 03:03:47 +0200

    ann optimization run complete

--- ann.py
+++ ann.py
@@ -18,6 +18,7 @@ from rpy2.robjects import pandas2ri
 from rpy2.robjects.conversion import localconverter
 import requests
 from sklearn.decomposition import PCA
+from threading import Thread
 
 data_url = "https://nextcloud.sdu.dk/index.php/s/Zzjcqjopy5cTawn/download/data_33.Rdata"
 data_file = "data.Rdata"
@@ -36,6 +37,21 @@ r_data=r.load(data_file)
 
 with localconverter(ro.default_converter + pandas2ri.converter):
     ciphers = ro.conversion.rpy2py(r['ciphers'])
+
+def take(arr, indexes):
+    return arr[indexes]
+
+def take_inverse(arr, indexes):
+    mask = np.ones(len(arr), dtype=bool)
+    mask[indexes] = False
+    return arr[mask]
+
+def all_persons_in(ciphers, split = 0.5):
+    count = len(ciphers)
+    samples = random.Random(666).sample(range(0, count), int(count * split))
+    train_data = take(ciphers, samples)
+    test_data = take_inverse(ciphers, samples)
+    return { "train_data": train_data, "test_data": test_data }
 
 def generate_pca(ciphers, cumsum):
     data = ciphers[:, 2:]
@@ -78,69 +94,134 @@ def F1(y_true, y_pred):
 
 def to_class_matrix(labels):
     numbers = set(labels)
-    result = [[]] * len(labels)
+    result = [[0] * len(numbers)] * len(labels)
     for i, label in enumerate(labels):
-        vector = [0] * len(numbers)
+        vector = result[i]
         for j in range(0, len(vector)):
             if (label == j):
-                vector[j] = 1
+                vector[j] = 1.0
             else:
-                vector[j] = 0
+                vector[j] = 0.0
         result[i] = vector
-    return result
+    return np.array(result)
 
 data = ciphers_pca
 class_matrix = to_class_matrix(data[:,1].astype(int))
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-n_features = len(data.T)-2
-n_outputs = len(class_matrix[0])
+def generate_model(num_inputs, num_outputs, num_hidden_layers, activation_function):
 
-model = models.Sequential(name="DeepNN", layers=[
-    ### hidden layer 1
-    layers.Dense(name="h1", input_dim=n_features,
-                 units=int(round((n_features+1)/2)), 
-                 activation='sigmoid'),
-    
-    ### hidden layer 2
-    layers.Dense(name="h2", units=int(round((n_features+1)/4)), 
-                 activation='sigmoid'),
-    
-    ### layer output
-    layers.Dense(name="output", units=n_outputs, activation='sigmoid')
-])
-model.summary()
+    network_layers = []
+    network_layers.append(layers.InputLayer(input_shape=(num_inputs,)))
 
-model.compile(optimizer='adam', loss=loss_fn, 
-              metrics=['accuracy',F1])
+    for i in range(0, num_hidden_layers):
+        nodes = int(np.interp(i, [0, num_hidden_layers], [num_inputs, num_outputs]))
+        network_layers.append(layers.Dense(
+                name = "h{0}".format(i),
+                units=nodes,
+                activation=activation_function
+            ))
 
-print("training..")
-training = model.fit(x=data[:, 2:], y=class_matrix, batch_size=32, epochs=5, validation_split=0.3)
+    network_layers.append(layers.Dense(name="output", units=num_outputs, activation=activation_function))
 
-# plot
-metrics = [k for k in training.history.keys() if ("loss" not in k) and ("val" not in k)]    
-fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(15,3))
+    model = models.Sequential(name="CipherPerceptron", layers=network_layers)
+    return model
 
-## training    
-ax[0].set(title="Training")    
-ax11 = ax[0].twinx()    
-ax[0].plot(training.history['loss'], color='black') 
-ax[0].set_xlabel('Epochs')    
-ax[0].set_ylabel('Loss', color='black')    
-for metric in metrics:        
-    ax11.plot(training.history[metric], label=metric)    
-ax11.set_ylabel("Score", color='steelblue')    
-ax11.legend()
-        
-## validation    
-ax[1].set(title="Validation")    
-ax22 = ax[1].twinx()    
-ax[1].plot(training.history['val_loss'], color='black')    
-ax[1].set_xlabel('Epochs')    
-ax[1].set_ylabel('Loss', color='black')    
-for metric in metrics:          
-    ax22.plot(training.history['val_'+metric], label=metric)   
-ax22.set_ylabel("Score", color="steelblue")
+def train_network (ciphers, activation_function, dense_layers, **kwargs):
+    train_data = ciphers[:, 2:]
+    train_labels = ciphers[:, 1]
 
-plt.savefig("plots/ann/training.png")
-plt.show()
+    n_features = len(data.T)-2
+    n_outputs = len(set(train_labels))
+
+    # Create model
+    print("Generating model with {0} features, {1} outputs, {2} dense layers, and activation function '{3}'..".format(n_features, n_outputs, dense_layers, activation_function))
+    model = generate_model(n_features, n_outputs, dense_layers, activation_function)
+    model.summary()
+
+    # Compile
+    print("Compiling model with {0} features, {1} outputs, {2} dense layers, and activation function '{3}'..".format(n_features, n_outputs, dense_layers, activation_function))
+    model.compile(optimizer='adam', loss=loss_fn, 
+        metrics=['accuracy',F1])
+
+    # Train
+    print("Training model with {0} features, {1} outputs, {2} dense layers, and activation function '{3}'..".format(n_features, n_outputs, dense_layers, activation_function))
+    training = model.fit(x=train_data, y=train_labels, **kwargs)
+    return (model, training)
+
+def train_layers(ciphers, function, dense_layers_range, **kwargs):
+    train_time = []
+    for dense_layers in dense_layers_range:
+
+        os.makedirs('anns/{0}/'.format(function), exist_ok=True)
+        os.makedirs('plots/ann/optimization/{0}/'.format(function), exist_ok=True)
+
+        sw = Stopwatch(True)
+        model, training = train_network(ciphers, function, dense_layers, **kwargs)
+        model.save('anns/{0}/{1}_layer.h5'.format(function, dense_layers))
+        time = sw.end()
+        train_time.append(time)
+
+        # plot
+        metrics = [k for k in training.history.keys() if ("loss" not in k) and ("val" not in k)]    
+        fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(15,3))
+
+        ## training    
+        ax[0].set(title="Training")    
+        ax11 = ax[0].twinx()    
+        ax[0].plot(training.history['loss'], color='black', label="loss") 
+        ax[0].set_xlabel('Epochs')    
+        ax[0].set_ylabel('Loss', color='black')    
+        for metric in metrics:        
+            ax11.plot(training.history[metric], label=metric)    
+        ax11.set_ylabel("Score", color='steelblue')    
+        ax11.legend()
+                    
+        ## validation    
+        ax[1].set(title="Validation")    
+        ax22 = ax[1].twinx()    
+        ax[1].plot(training.history['val_loss'], color='black', label="loss")    
+        ax[1].set_xlabel('Epochs')    
+        ax[1].set_ylabel('Loss', color='black')    
+        for metric in metrics:          
+            ax22.plot(training.history['val_'+metric], label=metric)   
+        ax22.set_ylabel("Score", color="steelblue") 
+
+        plt.savefig('plots/ann/optimization/{0}/{1}_layer.png'.format(function, dense_layers))
+
+    all_train_time[function] = train_time
+
+all_train_time = {}
+data = all_persons_in(ciphers_pca)["train_data"]
+
+activation_functions = ["sigmoid", "relu", "softmax", "softplus", "softsign", "tanh", "selu", "elu"]
+dense_layer_range = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+kwargs = { "batch_size": 32, "shuffle": True, "epochs": 100, "validation_split": 0.3 }
+
+use_threading = False
+if (not use_threading):
+    for function in activation_functions:
+        train_layers(data, function, dense_layer_range, **kwargs)
+else:
+    threads = [Thread(target=train_layers, args=(data, af, dense_layer_range), kwargs=kwargs)
+        for af in activation_functions]
+
+    # start the threads
+    for thread in threads:
+        thread.start()
+
+        # wait for the threads to complete
+    for thread in threads:
+        thread.join()
+
+plt.figure(figsize=(10,7))
+plt.title("Model training times")
+plt.xlabel("Dense layers")
+plt.ylabel("Time (seconds)")
+
+for function in all_train_time:
+    plt.plot(list(map(lambda x: str(x), dense_layer_range)), all_train_time[function], label=function)
+
+plt.legend()
+plt.savefig('plots/ann/optimization/computation_time.png')
